0bc49ad5581f36fb3a9d631d4fb6f210d8c17ee9
Renato Cruz <natorsc@gmail.com>
2025-10-08 19:29:29 -0300

    Adw.Sidebar Blueprint.

--- src/libadwaita-widgets/sidebar/ui/MainWindow.py
+++ src/libadwaita-widgets/sidebar/ui/MainWindow.py
@@ -24,12 +24,65 @@ blp_to_ui(file=BLP_FILE)
 Adw.init()
 
 
+class Page01(Adw.NavigationPage):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.set_title(title='Title page 01')
+        self.set_tag(tag='page-01')
+
+        adw_toolbar_view = Adw.ToolbarView.new()
+        self.set_child(child=adw_toolbar_view)
+
+        # Top bar.
+        adw_header_bar = Adw.HeaderBar.new()
+        adw_toolbar_view.add_top_bar(widget=adw_header_bar)
+
+        # Content.
+        adw_status_page = Adw.StatusPage.new()
+        adw_status_page.set_title(title='Page 01')
+        adw_toolbar_view.set_content(content=adw_status_page)
+        adw_toolbar_view.set_content(content=adw_status_page)
+
+
+class Page02(Adw.NavigationPage):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.set_title(title='Title page 02')
+        self.set_tag(tag='page-02')
+
+        adw_toolbar_view = Adw.ToolbarView.new()
+        self.set_child(child=adw_toolbar_view)
+
+        adw_header_bar = Adw.HeaderBar.new()
+        adw_toolbar_view.add_top_bar(widget=adw_header_bar)
+
+        adw_status_page = Adw.StatusPage.new()
+        adw_status_page.set_title(title='Page 02')
+        adw_toolbar_view.set_content(content=adw_status_page)
+
+
 @Gtk.Template(filename=UI_FILE)
 class ExampleWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'ExampleWindow'
 
+    adw_navigation_split_view = Gtk.Template.Child(
+        name='adw_navigation_split_view'
+    )
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.adw_navigation_split_view.set_content(content=Page01())
+
+    @Gtk.Template.Callback()
+    def on_sidebar_activated_cb(self, sidebar, index):
+        match sidebar.get_item(index).get_title():
+            case 'Item 01':
+                self.adw_navigation_split_view.set_content(content=Page01())
+            case 'Item 02':
+                self.adw_navigation_split_view.set_content(content=Page02())
+            case _:
+                pass
+        self.adw_navigation_split_view.set_show_content(show_content=True)
 
 
 class ExampleApplication(Adw.Application):
